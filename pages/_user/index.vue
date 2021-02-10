b3b81eb5ae5d63b30ce37263b2c484140dd76eec
chrisdixon161 <chrisdixon161@googlemail.com>
2021-02-10 17:12:03 +0000

    set up project query and routing

--- pages/_user/index.vue
+++ pages/_user/index.vue
@@ -1,6 +1,30 @@
 <template>
   <div>
     <h1>My Awesome Company</h1>
+    <h3>Projects</h3>
     <hr />
+    <div v-for="project in projects" :key="project.id">
+      <NuxtLink :to="`projects/${project.id}`">
+        <h3>{{ project.name }}</h3>
+      </NuxtLink>
+    </div>
   </div>
 </template>
+
+<script>
+import { projects } from '@/graphql/projects/queries'
+
+export default {
+  data() {
+    return {
+      projects: [],
+    }
+  },
+  apollo: {
+    $loadingKey: 'loading',
+    projects: {
+      query: projects,
+    },
+  },
+}
+</script>
--- pages/_user/projects/_id/index.vue
+++ pages/_user/projects/_id/index.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
     <h1>Project Name</h1>
+    <NuxtLink to="444/todos">
+      <article>
+        <h3>Todo's</h3>
+      </article>
+    </NuxtLink>
+    <article>
+      <h3>Chat</h3>
+    </article>
   </div>
 </template>
